da2920e986b6dac3c30bb70865d494b49fc84e23
Benjamin <benjamin.d42@gmail.com>
2017-03-06 22:59:42 +0100

    First function read_pixel

--- quadripartition.py
+++ quadripartition.py
@@ -1,7 +1,18 @@
-from PIL import Image
+"""This module does blah blah."""
 from math import sqrt
+from PIL import Image
 
-im = Image.open("licorne1.bmp")
-px = im.load()
+img = Image.open("resources/licorne1.bmp")
+pixels = img.load()
 
-print px
+def read_pixel(x, y):
+    """
+        Lit un pixels en valeur RGB
+
+        :param x:
+        :param y:
+        :rtype tuple(r, g, b):
+    """
+    return pixels[x, y]
+
+print read_pixel(2, 2)
